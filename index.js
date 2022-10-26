121f03767a3e4f784463acebe648328f7a29a986
walternomas <w.barriosvazquez@gmail.com>
2022-10-26 20:54:04 -0300

    routing

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
   res.send('hola mi server en express');
 });
 
+app.get('/nuevo', (req, res) => {
+  res.send('hola soy nuevo endpoint');
+});
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: "Walter",
+    price: 1000
+  });
+});
+
 app.listen(port, ()=> {
   console.log('listen on port', port)
 });
