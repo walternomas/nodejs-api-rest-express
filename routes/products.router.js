fcba844ea5be96159ae94cab620a8f4865453522
walternomas <w.barriosvazquez@gmail.com>
2022-10-27 08:43:29 -0300

    métodos put patch y delete

--- routes/products.router.js
+++ routes/products.router.js
@@ -38,4 +38,22 @@ router.post('/', (req, res) => {
   });
 });
 
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: "Product updated successfully",
+    data: body,
+    id,
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: "Product deleted successfully",
+    id,
+  });
+});
+
 module.exports = router;
