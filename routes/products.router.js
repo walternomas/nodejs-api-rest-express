8643db2824819b452a5773262d267ce1b17d111f
walternomas <w.barriosvazquez@gmail.com>
2022-10-27 08:53:01 -0300

    http response status code

--- routes/products.router.js
+++ routes/products.router.js
@@ -14,20 +14,26 @@ router.get('/', (req, res) => {
       image: faker.image.imageUrl()
     });
   }
-  res.json(products);
+  res.status(200).json(products);
 });
 
 router.get('/filter', (req, res) => {
-  res.send('yo soy un filter');
+  res.status(200).send('yo soy un filter');
 });
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  res.json({
-    id,
-    name: "Product 2",
-    price: 1000
-  })
+  if (id === '999') {
+    res.status(404).json({
+      message: 'not found'
+    });
+  } else {
+    res.status(200).json({
+      id,
+      name: "Product 2",
+      price: 1000
+    });
+  }
 });
 
 router.post('/', (req, res) => {
@@ -38,10 +44,20 @@ router.post('/', (req, res) => {
   });
 });
 
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.status(200).json({
+    message: "Product updated successfully",
+    data: body,
+    id,
+  });
+});
+
 router.patch('/:id', (req, res) => {
   const { id } = req.params;
   const body = req.body;
-  res.json({
+  res.status(200).json({
     message: "Product updated successfully",
     data: body,
     id,
@@ -50,7 +66,7 @@ router.patch('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   const { id } = req.params;
-  res.json({
+  res.status(200).json({
     message: "Product deleted successfully",
     id,
   });
