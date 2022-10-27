8ec7f6deeec2ec574d9dd66995327ab8a0a36965
walternomas <w.barriosvazquez@gmail.com>
2022-10-27 08:22:59 -0300

    separación de responsabilidades con express.Router

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const routerApi = require('./routes');
+
 const app = express();
 const port = 3000;
 
@@ -10,45 +12,8 @@ app.get('/nuevo', (req, res) => {
   res.send('hola soy nuevo endpoint');
 });
 
-app.get('/products', (req, res) => {
-  res.json([{
-    name: "Jabon",
-    price: 1000
-  },{
-    name: "detergente",
-    price: 2000
-  }]);
-});
+routerApi(app);
 
-app.get('/products/:id', (req, res) => {
-  const {id} = req.params;
-  res.json({
-    id,
-    name: "Product 2",
-    price: 1000
-  })
-});
-
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-  const {categoryId,productId} = req.params;
-  res.json({
-    categoryId,
-    productId
-  })
-});
-
-app.get('/users', (req, res) => {
-  const { limit, offset } = req.query;
-  if(limit && offset) {
-    res.json({
-      limit,
-      offset
-    });
-  }else {
-    res.send('no hay parametros');
-  }
-});
-
-app.listen(port, ()=> {
+app.listen(port, () => {
   console.log('listen on port', port)
 });
