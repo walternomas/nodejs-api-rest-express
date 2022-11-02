d467329ab9af8f9eefa4308b1a7a1cb35e02e5a4
Walter Barrios <w4l73r@DESKTOP-HARLOCK.localdomain>
2022-11-02 15:01:43 -0300

    deploy

--- index.js
+++ index.js
@@ -5,10 +5,25 @@ require('dotenv').config();
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
-const port = process.env.API_PORT;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors());
+
+const whitelist = [
+  'http://localhost:8080',
+  'http://localhost:3000',
+  'https://wbit.ar'
+];
+const options = {
+  origin: (origin, callback) => {
+    if(whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    }else {
+      callback(new Error('Not allowed'));
+    }
+  }
+};
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('hola mi server en express');
